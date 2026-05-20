43791980c470fefc20a9d458e83217944e8785c3
Francisco Coelho <franciscocoelho.ti@gmail.com>
2026-05-19 21:31:46 -0300

    Adicionando as routers put e delete da entidade Produto

--- sistema_estudos/routers/produtos.py
+++ sistema_estudos/routers/produtos.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter, status
-from sistema_estudos.schemas.produtos import ProdutoListPublicSchema, ProdutoPublicSchema,ProdutoSchema
+from sistema_estudos.schemas.produtos import ProdutoListPublicSchema, ProdutoPublicSchema, ProdutoSchema, ProdutoUpdateSchema
 from typing import Optional, List
 from sistema_estudos.db import PRODUTOS
 
@@ -13,9 +13,21 @@ router = APIRouter()
 def produto_list():
     return {'produtos': PRODUTOS}
 
+
 @router.post(path='/', response_model=ProdutoPublicSchema, status_code=status.HTTP_201_CREATED)
 def produto_create(produto: ProdutoSchema):
     produto_id = ProdutoPublicSchema(**produto.model_dump(), id=len(PRODUTOS)+1)
     PRODUTOS.append(produto_id)
     return produto_id
+
+@router.put(path='/{id: int}', response_model=ProdutoPublicSchema, status_code=status.HTTP_201_CREATED)
+def produto_update(id_produto: int, produto: ProdutoUpdateSchema):
+    produto_editado = ProdutoPublicSchema(**produto.model_dump(), id=id_produto)
+    PRODUTOS[id_produto - 1] = produto_editado
+    return produto_editado
+
+@router.delete(path='/{id: int}')
+def produto_delete(id_produto: int):
+    del PRODUTOS[id_produto - 1]
+    return 
 
--- sistema_estudos/schemas/produtos.py
+++ sistema_estudos/schemas/produtos.py
@@ -14,11 +14,13 @@ class ProdutoPublicSchema(BaseModel):
     quantidade: int
     valor: float
 
+# É usado Optional pelo fato dos campos serem opcionais a edição, o usuário pode editar apenas um campo (exemplo).
 class ProdutoUpdateSchema(BaseModel):
     descricao: Optional[str] = None
     quantidade: Optional[int] = None
     valor: Optional[float] = None
 
+# Cria o esquema de retorno de uma lista com todos os produtos
 class ProdutoListPublicSchema(BaseModel):
     produtos: List[ProdutoPublicSchema]
 
