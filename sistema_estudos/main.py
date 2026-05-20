5d09385201088cd341d1045aa2c08375c69ab720
Francisco Coelho <franciscocoelho.ti@gmail.com>
2026-05-20 08:36:35 -0300

    Ajustando os comentários no arquivo routers de Produto, e adicionando as excetions em produto_update e produto_delete

--- sistema_estudos/main.py
+++ sistema_estudos/main.py
@@ -1,10 +1,10 @@
 from fastapi import FastAPI
+# as (alias) se comporta como um apelido do elemento importando
 from sistema_estudos.routers.produtos import router as router_produtos
 
+# Objeto de FastAPI, permitindo o consumo de todos os recursos da biblioteca (framework)
 app = FastAPI()
-app.include_router(
-    router=router_produtos,
-    prefix='/api/produto',
-    tags=['produto']
-)
+
+# Como as rotas estão dentro do diretório routers, é necessário fazer a inclusão de todas as rotas no objeto app.
+app.include_router(router=router_produtos, prefix='/api/produto', tags=['produto'])
 
--- sistema_estudos/routers/produtos.py
+++ sistema_estudos/routers/produtos.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, status
+from fastapi import APIRouter, status, HTTPException
 from sistema_estudos.schemas.produtos import ProdutoListPublicSchema, ProdutoPublicSchema, ProdutoSchema, ProdutoUpdateSchema
 from typing import Optional, List
 from sistema_estudos.db import PRODUTOS
@@ -11,23 +11,36 @@ router = APIRouter()
 
 @router.get(path='/', response_model=ProdutoListPublicSchema, status_code=status.HTTP_200_OK)
 def produto_list():
+    # Retorna a lista com todos os produtos cadastrados
     return {'produtos': PRODUTOS}
 
 
 @router.post(path='/', response_model=ProdutoPublicSchema, status_code=status.HTTP_201_CREATED)
 def produto_create(produto: ProdutoSchema):
+    # A função model_dump() desestrutura o objeto para ser compreendido como um json
     produto_id = ProdutoPublicSchema(**produto.model_dump(), id=len(PRODUTOS)+1)
     PRODUTOS.append(produto_id)
     return produto_id
 
+
 @router.put(path='/{id: int}', response_model=ProdutoPublicSchema, status_code=status.HTTP_201_CREATED)
 def produto_update(id_produto: int, produto: ProdutoUpdateSchema):
+    # Verifica se o id_produto está maior no intervalo de 1 ao tamanho da lista, caso contrário dispara (raise) uma exception de "Usuário não encontrado".
+    if id_produto > len(PRODUTOS) or id_produto < 1:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='User Not Found')
+    
     produto_editado = ProdutoPublicSchema(**produto.model_dump(), id=id_produto)
     PRODUTOS[id_produto - 1] = produto_editado
     return produto_editado
 
+
 @router.delete(path='/{id: int}')
 def produto_delete(id_produto: int):
+    # Mesma validação realizada no produto_update
+    if id_produto > len(PRODUTOS) or id_produto < 1:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='User Not Found')
+    
+    # del deleta um determinado elemento da lista pelo seu índice
     del PRODUTOS[id_produto - 1]
     return 
 
